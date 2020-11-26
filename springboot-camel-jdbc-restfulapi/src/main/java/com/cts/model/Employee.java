42a08f6da4fa425120921369b27091e2f007e1f9
Rubini-prabakaran <58455727+Rubini-prabakaran@users.noreply.github.com>
2020-11-26 12:17:10 +0530

    Handson

---
+++ springboot-camel-jdbc-restfulapi/src/main/java/com/cts/model/Employee.java
@@ -0,0 +1,40 @@
+package com.cts.model;
+
+public class Employee {
+	
+	private String empId;
+	private String empName;
+	private String empSal;
+	
+
+	public String getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpSal() {
+		return empSal;
+	}
+
+	public void setEmpSal(String empSal) {
+		this.empSal = empSal;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + "]";
+	}
+
+
+}
